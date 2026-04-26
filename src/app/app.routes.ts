8af6ba395e1ddc20d5c3f6f3327aca09f9fddb79
CreppyClownLongLegs <majda.horozovic@hotmail.com>
2026-04-26 17:09:56 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WorkwithusComponent } from './workwithus/workwithus.component';
+import { PortofolioComponent } from './portofolio/portofolio.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent, title: 'Your Creator Studio'},
+    {path: 'workwithus', component: WorkwithusComponent, title: 'Work With Us'},
+    {path: 'portfolio', component: PortofolioComponent, title: 'Portfolio'},
+    {path: 'about', component: AboutusComponent, title: 'About Us'},
+    {path: 'contact', component: ContactComponent, title: 'Contact Us'}
+];
