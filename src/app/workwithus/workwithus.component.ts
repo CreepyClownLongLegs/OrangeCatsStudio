b9e9e35973fe6db7a9bad56d1921e07c7d101b0f
CreppyClownLongLegs <majda.horozovic@hotmail.com>
2026-04-28 19:28:57 +0200

    some changes

--- src/app/workwithus/workwithus.component.ts
+++ src/app/workwithus/workwithus.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-workwithus',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './workwithus.component.html',
   styleUrl: './workwithus.component.css'
 })
